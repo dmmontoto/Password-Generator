6e9fe4865fa7e3f3bcf693b1c2ae898f42a9463c
dmmontoto <dmontoto@ucsd.edu>
2023-07-05 23:46:31 +0100

    Added prompt to confirm password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,16 @@
 // Assignment code here
+function generatePassword() {
+  // Prompt a password length
+  var passwordLength = prompt("Enter the desired password length (between 8 and 128 characters):");
 
+  // Check previous password length input
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    // Exit function due to invalid input 
+    return;
+  }
+
+  // Prompts for preferred character types
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
